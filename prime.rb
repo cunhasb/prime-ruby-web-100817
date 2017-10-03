9f8bdaf6431794a8f5521fc26c9d1af24044ad7b
cunhasb CUNHA <cunhasb@aol.com>
2017-10-03 02:38:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+#trial division method
+
+def prime?(number)
+  array=[]
+  divisor=2
+  until divisor**2 >= number
+    array << divisor
+    divisor +=1
+  end
+  array
+end
+
+    
